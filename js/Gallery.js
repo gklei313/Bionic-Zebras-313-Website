accfa38f1f123570edfb87a461deace6873f014e
maureenr1987 <maureenr1987@gmail.com>
2019-01-07 15:24:40 -0500

    Optimized pictures

--- js/Gallery.js
+++ js/Gallery.js
@@ -6,21 +6,51 @@ var Pictures = [
 ];
 
 var CurrentPic = 0;
+var PicBuffer = "";
 
+//Format divs and pics
+for (i = 0; i < Pictures.length; i++) {
+    PicBuffer += "<img src='" + Pictures[i] + "' width='100%' alt='' id='picture_" + i + "'></img>"
+}
+
+//Add new divs
+document.getElementById("picture_frame").innerHTML = PicBuffer;
+
+
+//Set next pic
 function NextPic() {
     CurrentPic++
     if (CurrentPic >= Pictures.length) {
         CurrentPic = 0;
     }
-    document.getElementById("gallery_picture").src = Pictures[CurrentPic];
+    RefreshPics();
 }
 
+//Set previous pic
 function PrevPic() {
     CurrentPic--
     if (CurrentPic < 0) {
         CurrentPic = Pictures.length - 1;
     }
-    document.getElementById("gallery_picture").src = Pictures[CurrentPic];
+    RefreshPics();
 }
 
-setInterval( NextPic, 4000 );
+//Hide unused pictures and show current pic
+function RefreshPics () {
+    for (i = 0; i < Pictures.length; i++) {
+
+        if ( i == CurrentPic ) {
+        document.getElementById("picture_" + i).hidden = false;
+        }
+    
+        else {
+            document.getElementById("picture_" + i).hidden = true;
+        }
+    }
+}
+
+//Start
+RefreshPics();
+
+//Change pic every 4 secs
+setInterval(NextPic, 4000);
